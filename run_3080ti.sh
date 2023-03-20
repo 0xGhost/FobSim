88d90516dde345db7f1004b853686b64e269f1d9
root <root@DESKTOP-OGPLQC1.localdomain>
2023-03-20 21:20:26 +0000

    3080data for transaction pending time update

--- run_3080ti.sh
+++ run_3080ti.sh
@@ -22,30 +22,15 @@
 # 5: Delegated Proof of Stake (DPoS)
 # 6: Example New CA
 
-txPerBlock=(50)
+txPerBlock=(5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95)
 #txPerBlock=(5 10 15 20 25 30)
 #txPerBlock=(2 4)
 #txPerBlock=(2 4 8 16 32)
 #injectionRate=(256)
 #injectionRate=(2 4 8 16 32 64 128 256 512 1024)
-#injectionRate=(256 512 1024 2048 4096 8192 16384)
-injectionRate=(2048
-2176
-2304
-2432
-2560
-2688
-2816
-2944
-3072
-3200
-3328
-3456
-3584
-3712
-3840
-3968
-4096)
+injectionRate=(256 512 1024 2048 4096 8192 16384 32768)
+#injectionRate=(2048)
+
 
 for k in "${txPerBlock[@]}" # k: tx per block
 do
