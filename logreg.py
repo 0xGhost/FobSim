104f351745a85d33c3db5ecb47837f223aca18bc
0xGhost <root@AMD3070Ti.localdomain>
2023-03-24 13:33:04 +0000

    create logreg_input.xlsx, update old version table of target column name

--- logreg.py
+++ logreg.py
@@ -4,26 +4,39 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import sys
 
-
-# Load data from xlsx file
+#init global var
 xlsx_file = ' '
+old_table = True
+
+#handle console arguments
 if len(sys.argv) == 2:
     xlsx_file = sys.argv[1]
 else:
-    xlsx_file = '[3080ti]result copy 4.xlsx'
+    xlsx_file = 'logreg_input.xlsx'
+
+
+# Load data from xlsx file
 data = pd.read_excel(xlsx_file, engine='openpyxl')
+
+
 
 # Function to convert numbers to True if greater than 0 and False if less than or equal to 0
 def to_bool(x):
     return x > 0
 
 # Column you want to apply the function to
+
 column_to_convert = 'fail time(secs)'
+if old_table:
+    column_to_convert = 'queue too long (fail) time(secs)'
 data[column_to_convert] = data[column_to_convert].apply(to_bool)
 
 # Replace these with the appropriate column names
 feature_columns = ['injection rate(per sec)', 'tx per block']
 target_column = 'fail time(secs)'
+if old_table:
+    target_column = 'queue too long (fail) time(secs)'
+
 
 # Print the entire DataFrame
 #print("Data:")
